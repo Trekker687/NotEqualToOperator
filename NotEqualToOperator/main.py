4225b61a32972cb93afe35bcd0c8af3a13a70139
Trekker687 <113757673+Trekker687@users.noreply.github.com>
2024-11-11 17:31:23 +0530

    Add files via upload

---
+++ NotEqualToOperator/main.py
@@ -0,0 +1,25 @@
+a = 10
+b = 8
+c = 8
+
+print(a != b)
+print(c !=b)
+
+x= "coding"
+y = "code"
+
+if x != y:
+    print(x ,"and" ,y ,"are different")
+
+q = 4
+g = 5
+
+if (q == 1) != (g==5):
+    print("Hello")
+
+s = int(input("Enter any number"))
+
+if s%2 != 0:
+    print("The number is not even")
+else:
+    print("The number is even")
